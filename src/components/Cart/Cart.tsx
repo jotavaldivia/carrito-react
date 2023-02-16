294c83283f9cb18edb5d9dee8aa4c17af50721a3
unknown <juanvaldebenitovaldivia@outlook.es>
2023-02-16 15:13:13 -0300

    feat: add tailwindcss and components (HOME,HEADER,NAVBAR)

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import carrito from '../../assets/carrito.png'
+const Cart = () => {
+  return (
+    <>
+    <img src={carrito} alt="" width="50px"  height="50px"/>
+    </>
+  )
+}
+
+export default Cart
